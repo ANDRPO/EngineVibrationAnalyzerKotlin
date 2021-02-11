7781fbc9de798e9432bfa0cde55c4981d0b17582
Andrpo <xacker_228@mail.ru>
2021-02-11 16:10:42 +0700

    after merge on test

--- app/src/main/java/com/papmobdev/enginevibrationanalyzerkotlin/presentation/feature/ViewBindingAdapters.kt
+++ app/src/main/java/com/papmobdev/enginevibrationanalyzerkotlin/presentation/feature/ViewBindingAdapters.kt
@@ -8,16 +8,6 @@ import androidx.databinding.BindingAdapter
 import androidx.databinding.adapters.TextViewBindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-@BindingAdapter(value = ["setAdapter"])
-fun RecyclerView.bindRecyclerViewAdapter(adapter: RecyclerView.Adapter<*>) {
-    this.adapter = adapter
-}
-
-@BindingAdapter(value = ["onTextChanged"])
-fun EditText.onTextChanged(onTextChanged: TextWatcher) {
-    this.addTextChangedListener(onTextChanged)
-}
-
 @BindingAdapter(value = ["setSelectedItemPosition"])
 fun AppCompatSpinner.setSelectedItemPosition(position: Int) {
     if (position-1 <= this.count) {
